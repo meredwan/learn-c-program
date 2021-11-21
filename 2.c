d7aa680dd115467be534d18eac5cc36c3dac27c1
meredwan <redoansr70@gmail.com>
2021-11-21 08:55:04 -0800

    first commit

---
+++ 2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+
+    int c = 10;
+    printf("%d",c);
+    return 0;
+}
---
+++ 3.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+    int num=100;
+    printf("%d ",num);
+}
